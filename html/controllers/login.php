ad130ad1b18511d2bdb211828d05c446acfd25e9
GonzaloOSierra <gonzalosierra1309@gmail.com>
2023-08-28 19:27:40 -0300

    login_3

--- html/controllers/login.php
+++ html/controllers/login.php
@@ -11,6 +11,28 @@ class Login extends SessionController {
         error_log('Login-> CARGA EL INDEX LOGIN');
         $this->view->render('login/index');
     }
+
+
+    function authenticate() { // nuevo
+        if($this -> existPOST(['username', 'password'])){
+            $username = $this->getPOST('username');
+            $password = $this->getPOST('password');
+            if($username == '' || empty($username) || $password == '' || empty($password)){
+                $this->redirect('', ['error' -> ErrorMessages::ERROR_LOGIN_AUTHENTICATE_EMPTY]);
+            }
+
+            $user = $this->model->login($username, $password);
+
+            if($user != NULL){
+                $this->initialize($user);
+            }else{
+                $this->redirect('', ['error' -> ErrorMessages::ERROR_LOGIN_AUTHENTICATE_DATA]);
+            }
+        }else{
+            $this->redirect('', ['error' -> ErrorMessages::ERROR_LOGIN_AUTHENTICATE]);
+        }
+        
+    }
 }
 
 ?>
