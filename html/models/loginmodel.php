475bdd84f4224facd829dc854c5d571d1f45267b
GonzaloOSierra <gonzalosierra1309@gmail.com>
2023-08-28 19:35:50 -0300

    login_4

--- html/models/loginmodel.php
+++ html/models/loginmodel.php
@@ -6,6 +6,30 @@ class LoginModel extends Model{
         parent::__construct();
     }
 
+    function login($username, $password){//nuevo
+        try {
+            $query = $this->prepare('SELECT * FROM users WHERE username = :username');
+            $query->execute(['username' => $username]);
+            
+            if($query->rowCount() == 1){
+                $item = $query->fetch(PDO::GETCH_ASSOC)
+                
+                $user = new UserModel();
+                $user->from($item);
+
+                if(password_verify($password, $user->getPassword())){
+                    error_log('LoginModel::login->success');
+                    return $user;
+                }else{
+                    return NULL;
+                    error_log('LoginModel::login->PASSWORD NO ES IGUAL');
+                }
+            }
+        } catch (PDOException $e) {
+            error_log('LoginModel::login->exception' . $e);
+            return NULL;
+        }
+    }
 }
 
 
