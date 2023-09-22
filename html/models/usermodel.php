370bc0785cb69488f42fa3582bf929262eb7c9c1
LAG13-OFC-PC <luka17sas@gmail.com>
2023-09-22 03:47:16 -0300

    mejoras y actualizacion en las consultas a la DB

--- html/models/usermodel.php
+++ html/models/usermodel.php
@@ -6,12 +6,15 @@ class UserModel extends Model implements IModel{
     private $username;
     private $password;
     private $role;
+    private $roleName;
     private $state;
+   
 
     public function __construct() {
        parent::__construct();
        $this->username = '';
        $this->password = '';
+       $this->roleName = '';
        $this->role = 0;
        $this->name = '';
        $this->state = 0;
@@ -19,7 +22,7 @@ class UserModel extends Model implements IModel{
     }
     public function save(){
         try{
-            $query-> $this->prepare('INSERT users(username, password, id_rol, state) VALUES (:username, :password, :role, :state)');
+            $query-> $this->prepare('INSERT employees(username, password, id_rol, state) VALUES (:username, :password, :role, :state)');
             $query->execute([
                 'username' => $this->username,
                 'password' => $this->password,
@@ -36,14 +39,17 @@ class UserModel extends Model implements IModel{
     public function getAll(){
         $items = [];
         try{
-            $query = $this->query('SELECT * FROM users');
+            $query = $this->query('SELECT employees.*, rols.rol_name
+                                    FROM employees
+                                    JOIN rols ON employees.id_rol = rols.id_rol;');
             while($p = $query->fetch(PDO::FETCH_ASSOC)){
                 $item = new UserModel();
-                $item->setId($p['id_user']);
+                $item->setId($p['id_employee']);
                 $item->setUsername($p['username']);
                 $item->setPassword($p['password']);
                 $item->setRole($p['id_rol']);	
-                $item->setName($p['state']);
+                $item->setState($p['state']);
+                $item->setRoleName($p['rol_name']);
 
                 array_push($items ,$item);
             }
@@ -57,7 +63,10 @@ class UserModel extends Model implements IModel{
 
     public function get($id){
         try{
-            $query = $this->prepare('SELECT * FROM users WHERE id_user = :id');
+            $query = $this->prepare('SELECT employees.*, rols.rol_name
+                                        FROM employees
+                                        JOIN rols ON employees.id_rol = rols.id_rol
+                                        WHERE employees.id_employee = :id;');
             $query->execute([
                 'id' => $id,
             ]);
@@ -70,8 +79,10 @@ class UserModel extends Model implements IModel{
             $this->setUsername($user['username']);
             $this->setPassword($user['password']);
             $this->setRole($user['id_rol']);	
-            $this->setRole($user['state']);
-            $this->setId($user['id_user']);
+            $this->setRoleName($user['rol_name']);	
+            $this->setState($user['state']);
+            $this->setId($user['id_employee']);
+            $this->setName($user['name']);
             
             return $this;
         }catch(PDOException $e){
@@ -80,7 +91,7 @@ class UserModel extends Model implements IModel{
     }
     public function delete($id){
         try{
-            $query = $this->prepare('DELETE * FROM users WHERE id_user = :id');
+            $query = $this->prepare('DELETE * FROM employees WHERE id_employee = :id');
             $query->execute([
                 'id' => $id,
             ]);
@@ -92,9 +103,9 @@ class UserModel extends Model implements IModel{
     }
     public function update(){
         try{
-            $query = $this->prepare('UPDATE users SET username = :username, password = :password, id_rol = :role, state = :state WHERE id_user = :id');
+            $query = $this->prepare('UPDATE employees SET username = :username, password = :password, id_rol = :role, state = :state WHERE id_user = :id');
             $query->execute([
-                'id_user' => $this->$id,
+                'id_employee' => $this->$id,
                 'username' => $this->$username,
                 'id_rol' => $this->$role,
                 'password' => $this->$password,
@@ -109,17 +120,19 @@ class UserModel extends Model implements IModel{
         }
     }
     public function from($array){
-        $this->id = $array['id_user'];
+        $this->id = $array['id_employee'];
         $this->username = $array['username'];
         $this->password = $array['password'];
         $this->role     = $array['id_rol'];
+        $this->roleName     = $array['rol_name'];
         $this->state     = $array['state'];
+        $this->name    = $array['name'];
     }
 
     //verifica si existe usernamame en la base de datos
     public function exists($username){
         try{
-            $query = $this->prepare('SELECT username FROM users WHERE username = :username');
+            $query = $this->prepare('SELECT username FROM employees WHERE username = :username');
             $query->execute(['username' => $username]);
             if($query->rowCount() > 0){
                 return true;
@@ -150,17 +163,21 @@ class UserModel extends Model implements IModel{
     }
     public function setId($id){             $this->id = $id;  }
     public function setUsername($username){ $this->username = $username; }
+    public function setName($name){ $this->name = $name; }
     public function setPassword($password){ 
         $this->password = $this->getHashedPassword($password);
      }
     public function setRole($role){         $this->role = $role; }
+    public function setRoleName($roleName){         $this->roleName = $roleName; }
     public function setState($state){         $this->state = $state; }
 
     public function getId(){                return $this->id;}
     public function getUsername(){          return $this->username;}
+    public function getName(){          return $this->name;}
     public function getPassword(){          return $this->password;}
     public function getState(){              return $this->state;}
     public function getRole(){              return $this->role;}
+    public function getRoleName(){              return $this->roleName;}
 
 }
 ?>
