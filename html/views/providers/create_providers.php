0eb9e95eb18afb667abfe786eab05d03dd62a38a
Fabrusio <140295016+Fabrusio@users.noreply.github.com>
2023-10-10 16:34:25 -0300

    vista creación de proveedores

---
+++ html/views/providers/create_providers.php
@@ -0,0 +1,58 @@
+<?php
+include_once "/var/www/html/models/providermodel.php";
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$providerModel = new ProviderModel();
+$providers = $providerModel->getAll();
+
+$razonesSociales = array();
+foreach ($providers as $provider) {
+    $razonesSociales[] = $provider->getRazonSocial();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+    <body>
+        <div class="container">
+            <h2 class="text-center mb-4">Registro de Proveedor</h2>
+            <form method="post" action="http://localhost:8080/crud_providers/createProvider">
+                <div class="mb-3">
+                    <label for="providerName" class="form-label">Razón social:</label>
+                    <input type="text" class="form-control" name="providerName" id="providerName" required>
+                    <span id="providerName-error" style="color: red;"></span>
+                </div>
+                <div class="mb-3">
+                    <label for="CUIT" class="form-label">CUIT:</label>
+                    <input type="text" maxlength="11" oninput="this.value = this.value.replace(/[^0-9]/g, '');" class="form-control" name="CUIT" required>
+                </div>
+                <button type="submit" class="btn btn-primary">Enviar</button>
+            </form>
+        </div>
+    </body>
+</html>
+
+<script>
+    var existingNames = <?php echo json_encode($razonesSociales); ?>;
+    document.querySelector('form').addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        var provider = document.getElementById('providerName').value;
+        var providerError = document.getElementById('providerName-error');
+
+        if (existingNames.includes(provider)) {
+            providerError.innerText = 'El proveedor ya está registrado.';
+        } else {
+            this.submit();
+        }
+
+    });
+</script>
