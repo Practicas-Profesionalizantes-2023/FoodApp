6e112b68dd664b75b7800b81ec32b6446fbb3fdc
Fabrusio <fabrusio3p3@gmail.com>
2023-10-12 16:08:20 -0300

    Función delete y edit (falta modificar

--- html/controllers/crud_providers.php
+++ html/controllers/crud_providers.php
@@ -20,11 +20,50 @@ class Crud_providers extends SessionController {
 
             $providerModel = new ProviderModel();
             if ($providerModel->createProvider($providerName, $CUIT)) {
-                echo "Producto ingresado exitosamente.";
+                echo "Proveedor ingresado exitosamente.";
             } else {
-                echo "Error al crear el producto.";
+                echo "Error al crear el proveedor.";
             }
 
+        }
+    }
+
+    public function deleteProvider(){
+        if($this->existPOST(['id'])){
+            $id = $_POST['id'];
+            $providerModel = new ProviderModel();
+
+            if($providerModel->delete($id)){
+                echo "Proveedor eliminado exitosamente.";
+            }else{
+                echo "Error al eliminar el proveedor.";
+            }
+        }
+    }
+
+    public function editProviders(){
+        if($this->existPOST(['id', 'providerName', 'CUIT'])){
+            $id = $_POST["id"];
+            error_log('ESTE ES EL ID QUE RECIBE EN EL CONTROLADOR editProviders');
+            error_log($id);
+            $providerName = $_POST["providerName"]; 
+            $CUIT = $_POST["CUIT"];
+
+            $providerModel = new ProviderModel();
+            if ($providerModel->providerNameExists($id, $providerName, $CUIT)) {
+                echo "Ya existe ese proveedor.";
+                error_log('CONTROLADOR::PROVEEDORES-> EL USUARIO YA EXISTE TE TIRA ERROR');
+            } else {
+                error_log('CONTROLADOR::PROVEEDORES-> SE GUARDA LO RECIBIDO EN VARIAS VARIABLES');
+                $providerModel = new ProviderModel();
+
+                if ($providerModel->update($id, $productName, $stock)) {
+                    error_log('CONTROLADOR::PROVEEDORES-> SE ENVÍA');
+                } else {
+                    echo "Error al actualizar el Producto.";
+                    error_log('CONTROLADOR::PROVEEDORES-> HAY ALGÚN ERROR AL ACTUALIZAR EL PROVEEDOR');
+                }
+            }
         }
     }
 
