f6d1de3114cd91411923cccc603c060305086b51
LAG13-OFC-PC <luka17sas@gmail.com>
2023-12-08 23:54:47 -0300

    Correcciones de librerias

--- html/views/dashboard/dashboard.php
+++ html/views/dashboard/dashboard.php
@@ -12,9 +12,9 @@ h1 {
 
     </style>
 </head>
-<div class="card border-0 shadow-sm position-relative">
+<div class="card border-0 shadow-sm position-relative" style="display: flex; align-items: center; justify-content: center; height: 100vh;">
   <img class="card-img-top" src="../../public/img/hamburguesa2.avif" alt="Imagen comida">
-  <div class="position-absolute top-50 start-50 translate-middle p-3 bg-white text-center" style="opacity: 0.8;">
+  <div class="position-absolute p-3 bg-white" style="opacity: 0.8; text-align: center;">
     <h1 class="display-4 font-weight-bold">Bienvenido a APP FOOD</h1>
     <p class="lead font-weight-bold">Plataforma de venta de comidas rápidas</p>
   </div>
--- html/views/dashboard/index.php
+++ html/views/dashboard/index.php
@@ -34,37 +34,35 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- ============================================================================================================= -->
 
     <!-- Google Font: Source Sans Pro -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <link rel="stylesheet" href="views/assets/fonts/Source_Sans_3/sans3.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Kalam:wght@300;400;700&display=swap" rel="stylesheet">
-
+   
     <!-- Font Awesome Icons -->
     <link rel="stylesheet" href="views/assets/plugins/fontawesome-free/css/all.min.css">
 
     <!-- Ionicons -->
-    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <!-- <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"> -->
 
     <!-- SweetAlert2 -->
     <link rel="stylesheet" href="views/assets/plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css">
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+    <link rel="stylesheet" href="views/assets/plugins/toastr/toastr.css">
 
     <!-- Jquery CSS -->
     <link rel="stylesheet" href="views/assets/plugins/jquery-ui/css/jquery-ui.css">
 
     <!-- Bootstrap 5 -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="views/assets/dist/css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- JSTREE CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jstree/3.2.1/themes/default/style.min.css" />
-
+    
     <!-- ============================================================
     =ESTILOS PARA USO DE DATATABLES JS
     ===============================================================-->
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.11.0/css/jquery.dataTables.min.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.9/css/responsive.dataTables.min.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.0.0/css/buttons.dataTables.min.css">
+    <link rel="stylesheet" href="views/assets/datatables/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="views/assets/datatables/css/responsive.dataTables.min.css">
+    <link rel="stylesheet" href="views/assets/datatables/css/buttons.dataTables.min.css">
+   
 
     <!-- Theme style -->
     <link rel="stylesheet" href="views/assets/dist/css/adminlte.min.css">
@@ -91,7 +89,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- ChartJS -->
     <script src="views/assets/plugins/chart.js/Chart.min.js"></script>
 
-    <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+    <!-- <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script> -->
 
     <!-- InputMask -->
     <script src="views/assets/plugins/moment/moment.min.js"></script>
@@ -100,34 +98,32 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- SweetAlert2 -->
     <script src="views/assets/plugins/sweetalert2/sweetalert2.min.js"></script>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+    <script src="views/assets/plugins/toastr/toastr.min.js"></script>
 
     <!-- jquery UI -->
     <script src="views/assets/plugins/jquery-ui/js/jquery-ui.js"></script>
 
     <!-- JS Bootstrap 5 -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="views/assets/dist/js/bootstrap.bundle.min.js"></script>
 
 
-    <!-- JSTREE JS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jstree/3.2.1/jstree.min.js"></script>
-
+   
 
     <!-- ============================================================
     =LIBRERIAS PARA USO DE DATATABLES JS
     ===============================================================-->
-    <script src="https://cdn.datatables.net/1.11.0/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/responsive/2.2.9/js/dataTables.responsive.min.js"></script>
+    <script src="views/assets/datatables/js/jquery.dataTables.min.js"></script>
+    <script src="views/assets/datatables/js/dataTables.responsive.min.js"></script>
+
 
 
     <!-- ============================================================
     =LIBRERIAS PARA EXPORTAR A ARCHIVOS
     ===============================================================-->
-    <script src="https://cdn.datatables.net/buttons/2.0.0/js/dataTables.buttons.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/2.0.0/js/buttons.html5.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/2.0.0/js/buttons.print.min.js"></script>
-
+    <script src="views/assets/datatables/js/dataTables.buttons.min.js"></script>
+    <script src="views/assets/datatables/js/jszip.min.js"></script>
+    <script src="views/assets/datatables/js/buttons.html5.min.js"></script>
+    <script src="views/assets/datatables/js/buttons.print.min.js"></script>
     <!-- AdminLTE App -->
     <script src="views/assets/dist/js/adminlte.min.js"></script>
     <script src="views/assets/dist/js/plantilla.js"></script>
